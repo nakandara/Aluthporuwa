4624bd98069ce12d20573b3dc164b19ea3b11e9b
pramodniroshan <pramodnakandara@gmail.com>
2023-06-14 20:48:01 +0530

    add google auth

--- components/header.jsx
+++ components/header.jsx
@@ -1,23 +1,26 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useRouter } from "next/router";
-import LayOut from '../../components/Layout';
-import Sidebar from "../../components/Sidebar";
-import { useState } from "react";
+
 
 export default function Component() {
+
   const { data: session } = useSession();
   const router = useRouter();
 
   if (session && session.user) {
     return (
-      <>
-        <LayOut  />
-      </>
+      <div>
+<div style={{display:"flex",backgroundColor:"red", justifyContent:"flex-end"}}>
+        Signed in as {session.user.email} <br />
+        <button  onClick={() => signOut()}>Sign out</button>
+        
+      </div>
+      </div>
     );
   }
 
-  const handleSignIn = () =>
-    router.push(`/auth/signin?callbackUrl=${router.asPath}`);
+ 
+  const handleSignIn = () => router.push(`/auth/signin?callbackUrl=${router.asPath}`);
 
   return (
     <>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import LayOut from "../../components/Layout";
+import Sidebar from "../../components/Sidebar";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-const home = () => {
+const Home = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <h1>Loading...</h1>;
+  }
+
+  if (status === "unauthenticated") {
+    // Redirect the user to the sign-in page
+    router.push("/auth/signin");
+    return null;
+  }
+
   return (
-    <div>home</div>
-  )
-}
+    <div>
+      <LayOut>
+        <div style={{ margin: "100px", color: "red" }}>fnfkks</div>
+      </LayOut>
+    </div>
+  );
+};
 
-export default home
+export default Home;
