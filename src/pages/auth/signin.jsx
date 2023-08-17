31cf291cd1a83ad14eaf6f93b849808240d87412
pramodniroshan <pramodnakandara@gmail.com>
2023-08-17 13:39:25 +0530

    new one secoongg

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -1,152 +1,212 @@
-import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
-import { useToken } from '../../context/TokenContext';
-import axios from "axios";
+// import { signIn, useSession } from "next-auth/react";
+// import { useRouter } from "next/router";
+// import React, { useState } from "react";
+// import { useToken } from '../../context/TokenContext';
+// import axios from "axios";
 
-const provider = [
-  {
-    name: "google",
-  },
-];
+// const provider = [
+//   {
+//     name: "google",
+//   },
+// ];
+
+// const Signin = () => {
+
+//   const { token, setToken } = useToken();
+//   const router = useRouter();
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+
+  
+
+  
+
+//   // if (status === "loading") return <h1>Checking authentication</h1>;
+//   // if (session) {
+//   //   setTimeout(() => {
+//   //     router.push("/");
+//   //   }, 3000);
+//   //   return <div>You are already signed in</div>;
+//   // }
+//   console.log(process.env.BASE_URL);
+//   const handleOAuthSignIn = (name) => () => signIn(name);
+//   const google = () => {
+//     window.open(`/auth/google`);
+//   };
+//   console.log(process.env.BASE_URL);
+//   const normalLogin = async () => {
+//     const BASE_HOST_URL = process.env.BASE_HOST_URL;
+//     console.log(BASE_HOST_URL);
+//     try {
+//       const response = await axios.post(
+     
+//         `${process.env.BASE_HOST_URL}/api/login`,
+//         {
+//           email: username,
+//           password: password,
+//         }
+//       );
+  
+//       console.log(response, "fffffffffffffff");
+//       if (response.data) {
+//         localStorage.setItem("accessToken", response.data.token);
+//         // Store the token in local storage
+//         setToken(response.data.token);
+//         // Redirect the user after successful sign-in
+//         router.push("/home");
+//       } else {
+//         // Handle unsuccessful login (show an error message, etc.)
+//         console.log("Login failed");
+//       }
+//     } catch (error) {
+//       console.error("Error during login:", error);
+//     }
+//   };
+  
+  
+
+
+//   return (
+//     <div>
+    
+//       <form className="form" autoComplete="off" onSubmit={normalLogin}>
+//         <div className="control">
+//           <h1>Sign In</h1>
+//         </div>
+//         <div className="control block-cube block-input">
+//         <input
+//             name="username"
+//             type="text"
+//             placeholder="Username"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//           />
+//           <div className="bg-top">
+//             <div className="bg-inner"></div>
+//           </div>
+//           <div className="bg-right">
+//             <div className="bg-inner"></div>
+//           </div>
+//           <div className="bg">
+//             <div className="bg-inner"></div>
+//           </div>
+//         </div>
+//         <div className="control block-cube block-input">
+//         <input
+//             name="password"
+//             type="password"
+//             placeholder="Password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//           <div className="bg-top">
+//             <div className="bg-inner"></div>
+//           </div>
+//           <div className="bg-right">
+//             <div className="bg-inner"></div>
+//           </div>
+//           <div className="bg">
+//             <div className="bg-inner"></div>
+//           </div>
+//         </div>
+//         <button onClick={normalLogin} className="btn block-cube block-cube-hover" type="button">
+//           <div className="bg-top">
+//             <div className="bg-inner"></div>
+//           </div>
+//           <div className="bg-right">
+//             <div className="bg-inner"></div>
+//           </div>
+//           <div className="bg">
+//             <div className="bg-inner"></div>
+//           </div>
+//           {/* .bg2 */}
+//           <div  className="text">Log In</div>
+//         </button>
+//         {/* GOOGLE */}
+
+//         {provider.map(({ name }) => (
+         
+//           <button
+//             key={name}
+//             onClick={handleOAuthSignIn(name)}
+//             className="btn block-cube block-cube-hover"
+//             type="button"
+//           >
+//             <div className="bg-top">
+//               <div className="bg-inner"></div>
+//             </div>
+//             <div className="bg-right">
+//               <div className="bg-inner"></div>
+//             </div>
+//             <div className="bg">
+//               <div className="bg-inner"></div>
+//             </div>
+//             {/* .bg2 */}
+//             <div className="text">{name}</div>
+//           </button>
+//         ))}
+
+//         <div className="credits">
+//           <a href="https://codepen.io/marko-zub/" target="_blank"></a>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Signin;
+
+import React from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 const Signin = () => {
-
-  const { token, setToken } = useToken();
   const router = useRouter();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
 
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  
+    const email = e.target.email.value;
+    const password = e.target.password.value;
 
-  // if (status === "loading") return <h1>Checking authentication</h1>;
-  // if (session) {
-  //   setTimeout(() => {
-  //     router.push("/");
-  //   }, 3000);
-  //   return <div>You are already signed in</div>;
-  // }
-
-  const handleOAuthSignIn = (name) => () => signIn(name);
-  const google = () => {
-    window.open(`/auth/google`);
-  };
-
-  const normalLogin = async () => {
-    console.log(username);
     try {
-      const response = await axios.post(
-        "http://localhost:8080/api/login",
-        {
-          email: username,
-          password: password,
-        }
-      );
-  
-      console.log(response, "fffffffffffffff");
+      const response = await axios.post(`https://poruwa-back.onrender.com/api/createUser`, {
+        name: 'testone',
+        email: email,
+        password: password,
+      });
+
       if (response.data) {
-        localStorage.setItem("accessToken", response.data.token);
-        // Store the token in local storage
-        setToken(response.data.token);
-        // Redirect the user after successful sign-in
-        router.push("/home");
+        console.log('Logged in:', response.data);
+        router.push('https://web.facebook.com/reel/219207587757365');
       } else {
-        // Handle unsuccessful login (show an error message, etc.)
-        console.log("Login failed");
+        console.log('Login failed');
       }
     } catch (error) {
-      console.error("Error during login:", error);
+      console.error('Error during login:', error);
     }
   };
-  
-  
-
 
   return (
-    <div>
-    
-      <form className="form" autoComplete="off" onSubmit={normalLogin}>
-        <div className="control">
-          <h1>Sign In</h1>
+    <div className="container">
+      <img className="logo" src="https://www.edigitalagency.com.au/wp-content/uploads/Facebook-logo-blue-circle-large-transparent-png.png" alt="Facebook Logo" />
+      <form className="form" autoComplete="off" onSubmit={handleSubmit}>
+        <div className="input-container">
+          <input type="text" id="email" required />
+          <label htmlFor="email">Email or Phone</label>
         </div>
-        <div className="control block-cube block-input">
-        <input
-            name="username"
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <div className="bg-top">
-            <div className="bg-inner"></div>
-          </div>
-          <div className="bg-right">
-            <div className="bg-inner"></div>
-          </div>
-          <div className="bg">
-            <div className="bg-inner"></div>
-          </div>
+        <div className="input-container">
+          <input type="password" id="password" required />
+          <label htmlFor="password">Password</label>
         </div>
-        <div className="control block-cube block-input">
-        <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <div className="bg-top">
-            <div className="bg-inner"></div>
-          </div>
-          <div className="bg-right">
-            <div className="bg-inner"></div>
-          </div>
-          <div className="bg">
-            <div className="bg-inner"></div>
-          </div>
+        <button type="submit" className="login-button">Log In</button>
+        <div className="divider">
+          <div className="divider-line"></div>
+          <div className="divider-text">or</div>
+          <div className="divider-line"></div>
         </div>
-        <button onClick={normalLogin} className="btn block-cube block-cube-hover" type="button">
-          <div className="bg-top">
-            <div className="bg-inner"></div>
-          </div>
-          <div className="bg-right">
-            <div className="bg-inner"></div>
-          </div>
-          <div className="bg">
-            <div className="bg-inner"></div>
-          </div>
-          {/* .bg2 */}
-          <div  className="text">Log In</div>
-        </button>
-        {/* GOOGLE */}
-
-        {provider.map(({ name }) => (
-         
-          <button
-            key={name}
-            onClick={handleOAuthSignIn(name)}
-            className="btn block-cube block-cube-hover"
-            type="button"
-          >
-            <div className="bg-top">
-              <div className="bg-inner"></div>
-            </div>
-            <div className="bg-right">
-              <div className="bg-inner"></div>
-            </div>
-            <div className="bg">
-              <div className="bg-inner"></div>
-            </div>
-            {/* .bg2 */}
-            <div className="text">{name}</div>
-          </button>
-        ))}
-
-        <div className="credits">
-          <a href="https://codepen.io/marko-zub/" target="_blank"></a>
-        </div>
+        <a href="#" className="divider-text">Create New Account</a>
+        <a href="#" className="divider-text">Forgot Password?</a>
       </form>
     </div>
   );
