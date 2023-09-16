b9040997040c0b7436274d05ab2407f4998bf987
pramodniroshan <pramodnakandara@gmail.com>
2023-09-16 23:05:55 +0530

    add style

--- components/Home/MainPage.js
+++ components/Home/MainPage.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 
 // Your Video component with text overlay and opacity
@@ -17,13 +19,17 @@ const VideoWithTextAndOpacity = () => {
         muted
         style={{ opacity: 0.25 }} // Change this value to set the desired opacity (0 to 1)
       >
-         <source src="/media/heart.webm" type="video/webm" />
+         <source src="/media/poruwaimage.webm" type="video/webm" />
       
         Your browser does not support the video tag.
       </video>
       <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
       <h1 style={{ color: 'white', fontSize: '2rem', textShadow: '2px 2px 4px rgba(128, 128, 128, 0.5)' }}>
   <span className="responsive-text">ඔබගේ මංගල යෝජනාව දැන්ම වෙන් කරවා ගන්න.</span>
+  <br/>
+  <Button className='responsive_button' variant="contained" size="large">
+  ඉදිරියට යන්න.
+        </Button>
 </h1>
 
       </div>
@@ -41,6 +47,31 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function BasicGrid() {
   return (
-    <VideoWithTextAndOpacity />
+    <div>
+  <VideoWithTextAndOpacity />
+  <div  style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+  <Box
+    sx={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center', // Center horizontally
+      alignItems: 'center', // Center vertically
+      '& > :not(style)': {
+        m: 1,
+        width: 228,
+        height: 228,
+      },
+    }}
+  >
+     <Paper elevation={0} sx={{ backgroundColor: 'gray' }} /> {/* Change the background color to red */}
+    <Paper elevation={0} sx={{ backgroundColor: 'gray' }} /> {/* Change the background color to blue */}
+    <Paper elevation={0} sx={{ backgroundColor: 'gray' }} /> {/* Change the background color to green */}
+  </Box>
+  </div>
+  
+</div>
+
+    
+    
   );
 }
