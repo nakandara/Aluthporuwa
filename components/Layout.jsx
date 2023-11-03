f1fab24277d5c6bc76b5486092518fd50c960691
pramodniroshan <pramodnakandara@gmail.com>
2023-11-03 21:58:46 +0530

    create post body

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,95 +1,94 @@
-import React from 'react';
+import React from "react";
 
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import Container from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import AddHomeIcon from '@mui/icons-material/AddHome';
-import FolderSharedIcon from '@mui/icons-material/FolderShared';
-import AddIcCallIcon from '@mui/icons-material/AddIcCall';
-import { useRouter } from 'next/router';
-import Newsidebarandhome from '../components/newsidebarandhome'
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import Container from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import AddHomeIcon from "@mui/icons-material/AddHome";
+import FolderSharedIcon from "@mui/icons-material/FolderShared";
+import AddIcCallIcon from "@mui/icons-material/AddIcCall";
+import { useRouter } from "next/router";
+import Newsidebarandhome from "../components/newsidebarandhome";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
 import Dialog from "@mui/material/Dialog";
 import Slide from "@mui/material/Slide";
 
-import Menu from '@mui/material/Menu';
-import { useMediaQuery } from '@mui/material';
-import MenuItem from '@mui/material/MenuItem';
+import Menu from "@mui/material/Menu";
+import { useMediaQuery } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
-import YourPost from './Your-post/YourPost';
-
+import YourPost from "./Your-post/YourPost";
 
 const drawerWidth = 240;
-const navItems = ['Home', 'About', 'Contact', 'Login','MyAccount'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+const navItems = ["Home", "About", "Contact", "Login", "MyAccount"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: `-${drawerWidth}px`,
     ...(open && {
-      transition: theme.transitions.create('margin', {
+      transition: theme.transitions.create("margin", {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
       marginLeft: 0,
     }),
-  }),
+  })
 );
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
+  justifyContent: "flex-end",
 }));
 
 const Layout = ({ children }) => {
   const router = useRouter();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
- 
+
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -97,10 +96,8 @@ const Layout = ({ children }) => {
 
   const dialogStyle = {
     width: "80%",
-    height:"80%", // Default width for larger screens
+    height: "80%", // Default width for larger screens
     maxWidth: "none", // Remove the max-width constraint
-  
-   
   };
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
@@ -117,7 +114,7 @@ const Layout = ({ children }) => {
     localStorage.clear();
 
     // Redirect to the home page
-    router.push('/');
+    router.push("/");
   };
   const handleDrawerClose = () => {
     setOpen(false);
@@ -125,20 +122,20 @@ const Layout = ({ children }) => {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const getIconForItem = (item) => {
     switch (item) {
-      case 'Home':
+      case "Home":
         return <AddHomeIcon />;
-      case 'About':
+      case "About":
         return <FolderSharedIcon />;
-        case 'Contact':
+      case "Contact":
         return <AddIcCallIcon />;
-        case 'Login':
+      case "Login":
         return <FolderSharedIcon />;
-        case 'MyAccount':
+      case "MyAccount":
         return <FolderSharedIcon />;
-    
+
       default:
         return null;
     }
@@ -155,11 +152,9 @@ const Layout = ({ children }) => {
     setMenuOpen(false);
   };
 
-
-
   const OpenMenu = () => {
     handleOpen();
-    console.log('open');
+    console.log("open");
   };
 
   // Function to close the menu
@@ -169,175 +164,171 @@ const Layout = ({ children }) => {
 
   return (
     <div>
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-          onClick={OpenMenu}
-            color="inherit"
-            aria-label="open drawer"
-          
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-           
-          </Typography>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 ,left:'75%',position: 'absolute',top:10, }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <YourPost/>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              onClick={OpenMenu}
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: "none" }) }}
             >
-             {settings.map((setting) => (
-                <MenuItem
-                  key={setting}
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div"></Typography>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton
+                  onClick={handleOpenUserMenu}
+                  sx={{ p: 0, left: "75%", position: "absolute", top: 10 }}
+                >
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <YourPost />
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem
+                    key={setting}
+                    onClick={() => {
+                      if (setting === "Logout") {
+                        handleLogout(); // Call the logout function
+                      } else {
+                        handleCloseUserMenu();
+                      }
+                    }}
+                  >
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "ltr" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {navItems.map((item) => (
+              <ListItem
+                key={item}
+                button
+                onClick={() => {
+                  if (item === "Login") {
+                    router.push("/auth/signin");
+                  } else {
+                    router.push(`/${item.toLowerCase()}`);
+                  }
+                  handleDrawerToggle();
+                }}
+              >
+                <ListItemIcon>{getIconForItem(item)}</ListItemIcon>
+                <ListItemText primary={item} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {["All mail", "Trash", "Spam"].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+
+        <Main open={open}>{children}</Main>
+        
+      </Box>
+      <div style={{padding:"100px"}}>footer</div>
+      <Dialog
+        open={menuOpen}
+        onClose={CloseMenu}
+        disableScrollLock
+        PaperProps={{ style: dialogStyle }}
+        className="profile_InfoPopup"
+        TransitionComponent={Slide} // Use the Slide transition
+        transitionDuration={600} // Adjust the duration as needed
+        transitionDirection="right" // Slide from right
+      >
+        <div className="profile_InfoPopup_one">
+          <div className="profile_InfoPopup_five">
+            <Button onClick={handleClose} color="primary">
+              Close
+            </Button>
+          </div>
+          <div className="hedding_menu">MENU</div>
+          <div>
+            <List>
+              {navItems.map((item) => (
+                <ListItem
+                  key={item}
+                  button
                   onClick={() => {
-                    if (setting === 'Logout') {
-                      handleLogout(); // Call the logout function
+                    if (item === "Login") {
+                      router.push("/auth/signin");
                     } else {
-                      handleCloseUserMenu();
+                      router.push(`/${item.toLowerCase()}`);
                     }
+                    handleDrawerToggle();
                   }}
                 >
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
+                  <ListItemIcon>{getIconForItem(item)}</ListItemIcon>
+                  <ListItemText primary={item} />
+                </ListItem>
               ))}
-            </Menu>
-          
-          </Box>
-        </Toolbar>
-    
-      </AppBar>
-  
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-      {navItems.map((item) => (
-        <ListItem
-          key={item}
-          button
-          onClick={() => {
-            if (item === 'Login') {
-              router.push('/auth/signin');
-            } else {
-              router.push(`/${item.toLowerCase()}`);
-            }
-            handleDrawerToggle();
-          }}
-        >
-          <ListItemIcon>{getIconForItem(item)}</ListItemIcon>
-          <ListItemText primary={item} />
-        </ListItem>
-      ))}
-    </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      
-      <Main open={open}>
-    {children}
-      </Main>
-      
-    </Box>
-    <Dialog
-  open={menuOpen}
-  onClose={CloseMenu}
-  disableScrollLock
-  PaperProps={{ style: dialogStyle }}
-  className="profile_InfoPopup"
-  TransitionComponent={Slide} // Use the Slide transition
-  transitionDuration={600} // Adjust the duration as needed
-  transitionDirection="right" // Slide from right
->
-      <div className="profile_InfoPopup_one">
-        <div className="profile_InfoPopup_five">
-        <Button onClick={handleClose} color="primary">
-            Close
-          </Button>
+            </List>
+          </div>
+          <div className="profile_InfoPopup_four">
+            <div className="profile_InfoPopup_two">1</div>
+            <div className="profile_InfoPopup_three">2</div>
+          </div>
         </div>
-        <div className='hedding_menu'>MENU</div>
-        <div>
-        <List>
-      {navItems.map((item) => (
-        <ListItem
-          key={item}
-          button
-          onClick={() => {
-            if (item === 'Login') {
-              router.push('/auth/signin');
-            } else {
-              router.push(`/${item.toLowerCase()}`);
-            }
-            handleDrawerToggle();
-          }}
-        >
-          <ListItemIcon>{getIconForItem(item)}</ListItemIcon>
-          <ListItemText primary={item} />
-        </ListItem>
-      ))}
-    </List>
-        </div>
-      <div className="profile_InfoPopup_four" >
-      <div className="profile_InfoPopup_two">1</div>
-        <div className="profile_InfoPopup_three">2</div>
-      </div>
-        
-      </div>
       </Dialog>
     </div>
-   
-    
-
   );
 };
 
