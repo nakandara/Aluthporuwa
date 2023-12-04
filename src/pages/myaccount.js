5db47b58bcac63787323ba80032526845f86702b
pramodniroshan <pramodnakandara@gmail.com>
2023-12-04 14:58:34 +0530

    refactor my account page

--- src/pages/myaccount.js
+++ src/pages/myaccount.js
@@ -1,193 +1,183 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Layout from "../../components/Layout";
 import axios from "axios";
-import { Container, Paper, Typography, Avatar } from '@mui/material';
-import Box from '@mui/material/Box';
+import { Container, Paper, Typography, Avatar } from "@mui/material";
+import Box from "@mui/material/Box";
 import { useRouter } from "next/router";
-import { styled } from '@mui/material/styles';
-import ProtectedRoute from "../../components/protect/protectedRoute"; 
-import MyInformations from "../../components/MyInformation/MyInformations"; 
+import { styled } from "@mui/material/styles";
+import ProtectedRoute from "../../components/protect/protectedRoute";
+import MyInformations from "../../components/MyInformation/MyInformations";
 import { environments } from "../../components/environment/environments";
-import { useToken } from '../context/TokenContext';
+import { useToken } from "../context/TokenContext";
+
+
+const url = `${environments.BASE_HOST_URL}/api/createProfilePhoto`
+const urlGet = `${environments.BASE_HOST_URL}/api/getProfilePhoto`
+const editUrl =  `${environments.BASE_HOST_URL}/api/editProfilePhoto`
 
 const MyAccount = () => {
-  const {  user } = useToken();
+  const { user } = useToken();
   const router = useRouter();
- 
 
   const [fileInputOpen, setFileInputOpen] = useState(false);
-  const [data, setData] = useState('');
-  const [image, setImage] = useState('');
+  const [data, setData] = useState("");
+  const [image, setImage] = useState("");
   const [loading, setLoading] = useState(true);
 
-  
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [postImage, setPostImage] = useState({ image: "" });
 
+  const fileInputRef = useRef(null);
 
-  const handleAvatarClick = () => {
-    setFileInputOpen(true);
-  };
-
-  const handleFileInputChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedFile(file);
-    if (file) {
-     
-      editAvatarPhoto(file);
-    } else {
-     
-      uploadProfilePhoto(file);
-    }
-  };
-
-  const editAvatarPhoto = async (file) => {
-    const formData = new FormData();
-    formData.append("image", file);
-    formData.append("userId", user.userId);
-
+  const fetchProfilePhoto = async () => {
     try {
-      const response = await axios.post(
-        `${environments.BASE_HOST_URL}/api/editProfilePhoto/${user.userId}`,
-        formData
-      );
+      const response = await axios.get(`${urlGet}/${user.userId}`);
 
-      if (response.data.success) {
-     
-      } else {
-        
+      if (response.data && response.data.length > 0) {
+        const photoUrl = response.data[0].image;
+        setPostImage({ image: photoUrl });
+        setAvatarSrc(photoUrl); // Set Avatar source here
       }
-    } catch (error) {
-      
-    }
-  };
-
-
-  const uploadProfilePhoto = async (file) => {
-    const formData = new FormData();
-    formData.append("image", file);
-    formData.append("userId", user.userId);
-  
-    try {
-      const response = await axios.post(
-        `${environments.BASE_HOST_URL}/api/createProfilePhoto`,
-        formData
-      );
-  
-      if (response.data.success) {
-   
-      } else {
-       
-      }
-    } catch (error) {
-    
-    }
+    } catch (error) {}
   };
 
   useEffect(() => {
-    if (user && user.userId) {
-      
-      const apiUrl = `${environments.BASE_HOST_URL}/api/getProfile/${user.userId}`;
-      const apiUrlPhoto = `${environments.BASE_HOST_URL}/api/getProfilePhoto/${user.userId}`;
-     
-      // Create an async function to fetch data
-      async function fetchData() {
-        try {
-          
-          const response = await axios.get(apiUrl);
-          const responsePhoto = await axios.get(apiUrlPhoto);
-          console.log(response.data?.profile);
-          if (!response.data) {
-            throw new Error('Network response was not ok');
-          }
-          const data = response.data?.profile;
-         
-          setData(data); // Update the state with the fetched data
-          setImage(responsePhoto.data?.imageUrl)
-          setLoading(false); // Set loading to false when data is loaded
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          setLoading(false); // Set loading to false in case of an error
-        }
-      }
-  
-      fetchData();
-    }
+    fetchProfilePhoto();
   }, [user]);
-  
 
+  const createPost = async (newImage) => {
+    try {
+      await axios.post(url, newImage);
+
+      fetchProfilePhoto();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const EditPost = async (newImage) => {
+    try {
+      await axios.post(`${editUrl}/${user.userId}`, newImage);
+
+      fetchProfilePhoto();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleFileUpload = async (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const base64 = await convertToBase64(file);
+      const userId = user.userId;
+      console.log(base64);
+      setPostImage({ ...postImage, image: base64, userId: user.userId });
+      EditPost({ image: base64, userId });
+    } else {
+      const base64 = await convertToBase64(file);
+      const userId = user.userId;
+      console.log(base64);
+      setPostImage({ ...postImage, image: base64, userId: user.userId });
+      createPost({ image: base64, userId });
+    }
+  };
+
+
+
+  
 
   const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(1),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   }));
 
   if (!user) {
     return <div>Loading user data...</div>;
   }
-console.log(image);
+  const handleButtonClick = () => {
+    fileInputRef.current.click(); // Programmatically trigger file input click
+  };
   return (
     <Layout>
       <ProtectedRoute>
-      <div className="multi-color-gradient" style={{ marginTop: "60px" }}>
-        <Box sx={{ width: 1 }}>
-          <Box display="grid" gridTemplateColumns={{ xs: "1fr", md: "repeat(12, 1fr)" }} gap={2}>
-            <Box gridColumn={{ xs: "1", md: "span 12" }} sx={{ height: '25vh', display: 'grid', marginBottom: "100px"}}>
-              <Item>
-                <label htmlFor="profile-photo-input">
-                  <Avatar
-                    sx={{ width: 120, height: 70, marginBottom: 2, cursor: 'pointer' }}
-                    alt="Profile Photo"
-                    src={image}
-                    onChange={handleFileInputChange}
-                    onClick={handleAvatarClick}
-                  />
-                </label>
-                <Typography>Hello, {user.name}!</Typography>
-                <Typography>Email: {user.email}</Typography>
-                <Typography variant="h6" gutterBottom>
-                  Upload Profile Photo
-                </Typography>
-              
-                {fileInputOpen && (
-                  <input
-                    accept="image/*"
-                    style={{ display: 'none' }}
-                    id="profile-photo-input"
-                    type="file"
-                    onChange={handleFileInputChange}
-                  />
-                )}
-              </Item>
-            </Box>
-            <Box gridColumn={{ xs: "1", md: "span 8" }}>
-              <Item>
-                <Typography>My Information Step 01</Typography>
-                <MyInformations name={user.name} data={data}/>
-              </Item>
-            </Box>
-            <Box gridColumn={{ xs: "1", md: "span 4" }}>
-              <Item>
-              <Typography>My Information Step 02</Typography>
-              <Typography> Name :  {data.username}</Typography>
-                <Typography> Birthday : {data.birthday}</Typography>
-                <Typography> Gender : {data.gender}</Typography>
-                <Typography> Religion : {data.religion}</Typography>
-                <Typography> Race : {data.race}</Typography>
-                <Typography>Caste :  {data.caste}</Typography>
-                <Typography> District : {data.district}</Typography>
-                <Typography>City : {data.city}</Typography>
-              </Item>
+        <div className="multi-color-gradient" style={{ marginTop: "60px" }}>
+          <Box sx={{ width: 1 }}>
+            <Box
+              display="grid"
+              gridTemplateColumns={{ xs: "1fr", md: "repeat(12, 1fr)" }}
+              gap={2}
+            >
+              <Box
+                gridColumn={{ xs: "1", md: "span 12" }}
+                sx={{ height: "25vh", display: "grid", marginBottom: "100px" }}
+              >
+                <Item>
+                  <div className="App">
+                    <form>
+                      <label
+                        htmlFor="file-upload"
+                        className="custom-file-upload"
+                        onClick={handleButtonClick}
+                      >
+                        <Avatar
+                          alt=""
+                          src={postImage.image || ""}
+                          style={{ height: "250px", width: "200px" }}
+                          className="uploaded-image"
+                        />
+                      </label>
+                      <input
+                        type="file"
+                        ref={fileInputRef}
+                        style={{ display: "none" }} // Hide the default file input
+                        accept=".jpeg, .png, .jpg"
+                        onChange={(e) => handleFileUpload(e)}
+                      />
+                    </form>
+                  </div>
+                </Item>
+              </Box>
+              <Box gridColumn={{ xs: "1", md: "span 8" }}>
+                <Item>
+                  <Typography>My Information Step 01</Typography>
+                  <MyInformations name={user.name} data={data} />
+                </Item>
+              </Box>
+              <Box gridColumn={{ xs: "1", md: "span 4" }}>
+                <Item>
+                  <Typography>My Information Step 02</Typography>
+                  <Typography> Name : {data.username}</Typography>
+                  <Typography> Birthday : {data.birthday}</Typography>
+                  <Typography> Gender : {data.gender}</Typography>
+                  <Typography> Religion : {data.religion}</Typography>
+                  <Typography> Race : {data.race}</Typography>
+                  <Typography>Caste : {data.caste}</Typography>
+                  <Typography> District : {data.district}</Typography>
+                  <Typography>City : {data.city}</Typography>
+                </Item>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </div>
+        </div>
       </ProtectedRoute>
-     
     </Layout>
   );
 };
 
 export default MyAccount;
+
+function convertToBase64(file) {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+}
