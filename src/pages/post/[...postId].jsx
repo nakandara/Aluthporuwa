7b835ec7cbd652d66cce4912432b0c2d14de9fe3
pramodniroshan <pramodnakandara@gmail.com>
2023-12-08 21:02:22 +0530

    add PostId

--- src/pages/post/[...postId].jsx
+++ src/pages/post/[...postId].jsx
@@ -5,16 +5,60 @@ import { environments } from "../../../components/environment/environments";
 import { useRouter } from "next/router";
 import { useToken } from '../../context/TokenContext';
 
-const PostId = () => {
+const PostId = ({ postIdData,loading }) => {
+ 
   return (
     <Layout>
- 
-  <div className="multi-color-gradient" style={{ marginTop: "150px" }}>
-wwwwwwwwwwwwwwwwwww
-  </div>
-
+      <div className="multi-color-gradient" style={{ marginTop: "70px", marginBottom: "40vh" }}>
+      <div className="card-container-post">
+        {loading?<> {postIdData?.allPosts?.map((post, index) => (
+            <div className="card-post" key={index} >
+              <div className="image_postView">
+                <img
+                  className="image_postView_image"
+                  src={post.PostDetails.image}
+                  alt={`Image ${index + 1}`}
+                />
+              </div>
+              <div className="description">{post.PostDetails.description}</div>
+            </div>
+          ))}</>:<>Loading..........</>}
+     
+        </div>
+      </div>
     </Layout>
-  )
+  );
+};
+
+export async function getServerSideProps(context) {
+
+  const { postId } = context.query; // Retrieve postId from the context object
+
+  
+  try {
+    let loading;
+    loading=false
+    const response = await axios.get(`${environments.BASE_HOST_LOCAL_URL}/api/getPost/${postId}`);
+   if (response) {
+   
+    loading = true
+   }
+    return {
+      props: {
+        postIdData: response.data,
+        loading // Pass the fetched data as props
+      }
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        postIdData: {} ,
+        loading
+        // Return empty object in case of error or no data
+      }
+    };
+  }
 }
 
-export default PostId
+export default PostId;
--- src/pages/post/index.jsx
+++ src/pages/post/index.jsx
@@ -8,22 +8,23 @@ import { useToken } from '../../context/TokenContext';
 const Post = () => {
   const { user } = useToken();
   const router = useRouter();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState('');
+  
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (user && user.userId) {
-      const apiUrl = `${environments.BASE_HOST_URL}/api/getPosts/${user.userId}`;
+      const apiUrl = `${environments.BASE_HOST_LOCAL_URL}/api/getPosts/${user.userId}`;
 
       async function fetchPostData() {
         try {
+          setLoading(false)
           const response = await axios.get(apiUrl);
-
-          if (!response.data || !Array.isArray(response.data.allPosts)) {
-            throw new Error('Invalid data format received from the API');
+          if (response.data) {
+            setLoading(true);
+            const posts = response.data;
+            setData(posts);
           }
-
-          const posts = response.data.allPosts;
-          setData(posts);
         } catch (error) {
           console.error('Error fetching data:', error);
         }
@@ -41,22 +42,23 @@ const Post = () => {
     return <div>Loading user data...</div>;
   }
 
+
   return (
     <Layout>
       <div style={{ marginTop: "70px", marginBottom: "40vh" }}>
         <div className="card-container-post">
-          {data?.map((post, index) => (
-            <div className="card-post" key={index} onClick={() => handleClick(post.PostDetails.postId)}>
+          {loading?<> {data?.map((post, index) => (
+            <div className="card-post" key={index} onClick={() => handleClick(post.postId)}>
               <div className="image_postView">
                 <img
                   className="image_postView_image"
-                  src={post.PostDetails.image}
+                  src={post.image}
                   alt={`Image ${index + 1}`}
                 />
               </div>
-              <div className="description">{post.PostDetails.description}</div>
+              <div className="description">{post.description.slice(0, 18)}</div>
             </div>
-          ))}
+          ))}</>:<>Loading...........</>}
         </div>
       </div>
     </Layout>
