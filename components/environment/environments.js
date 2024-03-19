d88aa2ce2fa569109cba24158ca2af6316f553bb
pramodniroshan <pramodnakandara@gmail.com>
2024-03-20 00:28:32 +0530

    change api url

--- components/environment/environments.js
+++ components/environment/environments.js
@@ -2,6 +2,6 @@
 export const environments = {
     // BASE_HOST_URL: "https://worldadd-api.vercel.app",
     BASE_HOST_LOCAL_URL:"http://localhost:8080",
-    BASE_HOST_URL: "http://54.90.243.118:8080",
+    BASE_HOST_URL: "http://52.73.121.161:8080",
   
 };
