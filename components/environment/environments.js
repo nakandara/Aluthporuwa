2b0cda89d7383de9289abdb5e85e5381faa231bb
pramodniroshan <pramodnakandara@gmail.com>
2024-03-28 23:39:41 +0530

    add env new url

--- components/environment/environments.js
+++ components/environment/environments.js
@@ -2,6 +2,6 @@
 export const environments = {
     // BASE_HOST_URL: "https://worldadd-api.vercel.app",
     BASE_HOST_LOCAL_URL:"http://localhost:8080",
-    BASE_HOST_URL: "http://52.73.121.161:8080",
+    BASE_HOST_URL: "http://ec2-34-227-111-145.compute-1.amazonaws.com:8080",
   
 };
