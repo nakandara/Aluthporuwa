5b3ed7468e9c028a361ba76fa378ffdee8769d95
pramodniroshan <pramodnakandara@gmail.com>
2024-04-06 01:40:08 +0530

    add new changes

--- components/Home/Content.jsx
+++ components/Home/Content.jsx
@@ -5,10 +5,10 @@ export default function Elevation() {
   return (
     <div className="container-fluid">
       <div className="row justify-content-center align-items-center">
-        <div className="col-md-4  mb-3">
+        <div className="col-md-4 mb-3">
           <div
             className="card border-success"
-            style={{ maxWidth: "18rem", maxWidth: "none" }}
+            style={{ maxWidth: "none" }} // Only keep maxWidth: "none"
           >
             <div className="card-header bg-transparent border-success">
               Header
@@ -17,7 +17,7 @@ export default function Elevation() {
               <h5 className="card-title">Success card title</h5>
               <p className="card-text">
                 Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                the bulk of the card&apos;s content.
               </p>
             </div>
             <div className="card-footer bg-transparent border-success">
@@ -28,7 +28,7 @@ export default function Elevation() {
         <div className="col-md-4 mb-3">
           <div
             className="card border-success"
-            style={{ maxWidth: "18rem", maxWidth: "none" }}
+            style={{ maxWidth: "none" }} // Only keep maxWidth: "none"
           >
             <div className="card-header bg-transparent border-success">
               Header
@@ -37,7 +37,7 @@ export default function Elevation() {
               <h5 className="card-title">Success card title</h5>
               <p className="card-text">
                 Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                the bulk of the card&apos;s content.
               </p>
             </div>
             <div className="card-footer bg-transparent border-success">
@@ -48,7 +48,7 @@ export default function Elevation() {
         <div className="col-md-4 mb-3">
           <div
             className="card border-success"
-            style={{ maxWidth: "18rem", maxWidth: "none" }}
+            style={{ maxWidth: "none" }} // Only keep maxWidth: "none"
           >
             <div className="card-header bg-transparent border-success">
               Header
@@ -57,7 +57,7 @@ export default function Elevation() {
               <h5 className="card-title">Success card title</h5>
               <p className="card-text">
                 Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                the bulk of the card&apos;s content.
               </p>
             </div>
             <div className="card-footer bg-transparent border-success">
